6c3a412d857f1bd7f9eb325e63a9c5936fe187b4
Tormod Kårstad <tormod.karstad@proserv.com>
2026-03-06 11:41:57 +0100

    frontend template

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -1,8 +1,16 @@
-from pydantic_settings import BaseSettings
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+ENV_PATH = Path(__file__).resolve().parents[2] / ".env" # parent[2] -> two folders back from current folder
 
 class Settings(BaseSettings):
-  database_url: str
-  redis_url: str
-  env: str = "dev"
+        database_url:str
+        redis_url: str
+
+        model_config = SettingsConfigDict(
+                env_file=str(ENV_PATH),
+                env_file_encoding="utf-8",
+                extra="ignore",
+        )
 
 settings = Settings()
